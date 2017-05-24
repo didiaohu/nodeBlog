bb0155a272e6cec975304b698acdf1e43bf5f01e
bob <301085344@qq.com>
2017-05-24 08:13:31 +0800

    提交最新代码

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+
+// router.get('/a', function(req, res, next) {
+//   res.render('hello');
+// });
+var crypto = require('crypto'),
+User = require('../models/user.js');
+
+module.exports = function(app){
+	app.get('/', function(req, res){
+		res.render('index', {title: 'Express'});
+	});
+	app.get('/reg', function(req, res){
+		res.render('reg', {title: '主页'});
+	});
+	app.post('/reg', function(req, res){
+		var name = req.body.name,
+			password = req.body.password,
+			password_re = req.body['password_repeat'];
+
+		if(password_re != password){
+			req.flash('error', '两次输入的密码不一致');
+			return res.redirect('/reg');
+		}
+		var md5 = crypto.createHash('md5'),
+			password = md5.update(req.body.password).digest('hex');
+		var newUser = new User({
+			name: req.body.name,
+			password: password,
+			email: req.body.email
+		});
+
+		User.get(newUser.name, function(err, user){
+			if(err){
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			if(user){
+				req.flash('error', '用户已存在！');
+				return res.redirect('/reg');
+			}
+			newUser.save(function(err, user){
+				if(err){
+					req.flash('error', err);
+					return res.redirect('/reg');
+				}
+				req.session.user = user;
+				req.flash('success', '注册成功！');
+				res.redirect('/');
+			});
+		});
+
+	});
+	app.get('/login', function(req, res){
+		res.render('login', {title: '登录'});
+	});
+	app.post('/login', function(req, res){
+
+	});
+	app.get('/post', function(req, res){
+		res.render('post', {title: '发表'});
+	});
+	app.post('/post', function(req, res){
+	});
+	app.get('/logout', function(req, res){
+	});
+};
---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+	cookieSecret: 'MongoDB',
+	db: 'blog',
+	host: 'localhost',
+	port: 27017
+}
+
