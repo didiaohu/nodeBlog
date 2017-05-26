0b83ba52250ee1ed62b4ce5ed09186a7d88b008e
bob <301085344@qq.com>
2017-05-26 21:33:01 +0800

    实现用户页面和文章页面

--- models/post.js
+++ models/post.js
@@ -51,7 +51,7 @@ Post.prototype.save = function(callback){
 }
 
 
-Post.get = function(name, callback){
+Post.getAll = function(name, callback){
 	mongodb.open(function(err, db){
 		if(err){
 			return callback(err);
@@ -84,3 +84,37 @@ Post.get = function(name, callback){
 	});
 };
 
+
+Post.getOne = function(name, day, title, callback){
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			
+			var query = {};
+			if(name){
+				query.name = name;
+			}
+
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			},function(err, doc){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+
+				doc.post = markdown.toHTML(doc.post);
+				callback(null, doc);
+			});
+		});
+	});
+};
