8151615bb5230ac83ba87c720880558b4ecb1784
bob <301085344@qq.com>
2017-05-28 19:42:10 +0800

    完成分页功能

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,36 @@ Comment = require('../models/comment.js');
 
 module.exports = function(app){
 	app.get('/', function(req, res){
-		Post.getAll(null, function(err, posts){
-			console.log(err)
+		var page = req.query.p ? parseInt(req.query.p) : 1;
+		// Post.getAll(null, function(err, posts){
+		// 	console.log(err)
+		// 	if(err){
+		// 		posts = [];
+		// 	}
+		// 	res.render('index', {
+		// 		title: '主页',
+		// 		user: req.session.user,
+		// 		posts: posts,
+		// 		success: req.flash('success').toString(),
+		// 		error: req.flash('error').toString()
+		// 	});
+			
+		// });
+
+		Post.getTen(null, page, function(err, posts, total){
 			if(err){
 				posts = [];
 			}
 			res.render('index', {
 				title: '主页',
-				user: req.session.user,
 				posts: posts,
+				page: page,
+				isFirstPage: (page - 1) == 0,
+				isLastPage: ((page - 1) * 10 + posts.length) == total,
+				user: req.session.user,
 				success: req.flash('success').toString(),
 				error: req.flash('error').toString()
 			});
-			
 		});
 	});
 
@@ -152,21 +169,23 @@ module.exports = function(app){
 	});
 
 	app.get('/u/:name', function(req, res){
+		var page = req.query.p ? parseInt(req.query.p) : 1;
 		User.get(req.params.name, function(err, user){
 			if(!user){
 				req.flash('error', '用户不存在！');
 				return res.redirect('/');
 			}
 
-			Post.getAll(user.name, function(err, posts){
+			Post.getTen(null, page, function(err, posts, total){
 				if(err){
-					req.flash('error', err);
-					return res.redirect('/');
+					posts = [];
 				}
-
-				res.render('user',{
+				res.render('user', {
 					title: user.name,
 					posts: posts,
+					page: page,
+					isFirstPage: (page - 1) == 0,
+					isLastPage: ((page - 1) * 10 + posts.length) == total,
 					user: req.session.user,
 					success: req.flash('success').toString(),
 					error: req.flash('error').toString()
